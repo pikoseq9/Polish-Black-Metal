d3c22417c7c25d3341b12cc4a39074a113c42d5d
pikoseq9 <wekpat@wp.pl>
2023-09-08 17:26:32 +0200

    innit

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"/>
+<body>
+    <menu>
+        <div class="menu-line"></div>
+        <div class="menu-box">
+            <div class="menu-content">
+                <a class="title">POLISH BLACK METAL</a>
+            </div>
+        </div>
+    </menu>
+    <div class="menu-image">
+        <img src="images/forest-bg.png"/>
+    </div>
+    <div class="main">
+        <div class="band-sector">
+            <div class="band-box">
+                <div class="band-text">
+                <h1 class="title">1. Behemoth</h1>
+                <a>Behemoth is a Polish extreme metal band renowned for its uncompromising blend of black and death metal. Founded in 1991 by frontman Adam "Nergal" Darski, the band has become an icon in the global metal scene, known for their theatrical stage presence, thought-provoking lyrics, and musical innovation.<br><br></a>
+                <a>Behemoth's sound is characterized by its sheer intensity, combining blistering guitar riffs, thunderous drumming, and Nergal's distinctive growling vocals. Over the years, they've evolved from their early raw black metal roots to a more polished, multifaceted sound that incorporates symphonic and progressive elements.<br><br></a>
+                <a>Their lyrical themes delve into esoteric and philosophical subjects, often exploring themes of occultism, spirituality, and existentialism. Nergal's lyrical and visual expressions are heavily influenced by his personal journey, as well as his battles with cancer and encounters with theocratic controversy in Poland.<br><br></a>
+                <a>Behemoth's discography includes critically acclaimed albums like "The Satanist" and "Demigod," showcasing their technical prowess and artistic depth. Their live performances are renowned for their grandeur and spectacle, often featuring elaborate costumes, pyrotechnics, and dark theatricality.<br><br></a>
+                <a>Behemoth's impact on the global metal community extends beyond music, as they push the boundaries of artistic expression and challenge societal norms. Their fearless approach to their craft and unapologetic exploration of the darker aspects of existence have solidified their position as one of the most influential and revered bands in extreme metal.<br><br></a>
+                </div>
+                <div class="band-image">
+                    <img src="images/band1.png"/>
+                </div>
+            </div>
+        </div>
+        <div class="menu-line"></div>
+        <div class="band-sector">
+            <div class="band-box">
+                <div class="band-image">
+                    <img src="images/band2.png"/>
+                </div>
+                <div class="band-text">
+                <h1 class="title">2. Profeci</h1>
+                <a>Profeci is a dynamic and innovative band hailing from Poznań. Comprising a group of highly talented musicians, they have made a significant impact on the contemporary music scene with their unique blend of musical styles and thought-provoking lyrics.<br><br></a>
+                <a>Mikis - Bass<br></a>
+                <a>Symeon - Drums<br></a>
+                <a>Gustaw - Guitars<br></a>
+                <a>Piołun - Guitars, Vocals<br></a>   
+                </div>
+            </div>
+        </div>
+        <div class="menu-line"></div>
+        <div class="band-sector">
+            <div class="band-box">
+                <div class="band-text">
+                    <h1 class="title">3. Batushka</h1>
+                    <a>Batushka is a unique and enigmatic black metal band that has captivated audiences with its unorthodox approach to the genre. Hailing from Poland, the band's identity has been shrouded in mystery, adding to their mystique and intrigue.<br><br></a>
+                    <a>One of the defining features of Batushka is their incorporation of Eastern Orthodox liturgical elements into their music. Their debut album, "Litourgiya," released in 2015, gained widespread attention for its fusion of black metal aggression with haunting chants, hymns, and religious themes. This blending of the sacred and the profane created a captivating and atmospheric experience that set them apart from traditional black metal acts.<br><br></a>
+                    <a>The band's live performances have been equally captivating, featuring elaborate rituals, ornate vestments, and a unique stage setup that reinforces their mystic aura.<br><br></a>
+                    <a>However, Batushka's journey has also been marked by internal conflicts and disputes over the band's identity and direction. These disputes have led to the existence of multiple incarnations of Batushka, each with its own interpretation of the band's vision.<br><br></a>
+                    <a>Despite the controversies, Batushka's music and live shows have left an indelible mark on the black metal scene, earning them a dedicated following. Their ability to push boundaries and challenge conventions has solidified their place as one of the most intriguing and polarizing acts in modern black metal, making them a subject of fascination and debate among metal enthusiasts worldwide.<br><br></a>   
+                </div>
+                <div class="band-image">
+                    <img src="images/band3.png"/>
+                </div>
+            </div>
+        </div>
+        <div class="menu-line"></div>
+        <div class="band-sector">
+            <div class="band-box">
+                <div class="band-image">
+                    <img src="images/band4.png"/>
+                </div>
+                <div class="band-text">
+                    <h1 class="title">4. Mgła</h1>
+                    <a>Mgła is a Polish black metal band that has garnered significant acclaim in the underground metal scene for their powerful and atmospheric music. The band's name, which means "fog" or "mist" in Polish, aptly captures the enigmatic and obscure nature of their sound and identity.<br><br></a>
+                    <a>Formed in 2000, Mgła consists of two core members, Mikołaj "M." Żentara and Maciej "Darkside" Kowalski, who have maintained a deliberate shroud of anonymity throughout their career. This anonymity has allowed their music to take center stage, devoid of personal identity or image distractions.<br><br></a>
+                    <a>Mgła's music is characterized by its intense and relentless approach to black metal. They are known for their precise and aggressive guitar work, pummeling drumming, and M.'s distinctive rasping vocals. Their compositions often feature intricate melodic elements, creating a bleak and atmospheric backdrop for their lyrical themes.<br><br></a>
+                    <a>Lyrically, Mgła explores themes of existentialism, nihilism, and the human condition. Their lyrics are delivered with a poetic and philosophical touch, delving into the complexities of life, death, and the void in between.<br><br></a>   
+                    <a>The band's discography includes several critically acclaimed albums, such as "With Hearts Toward None" and "Exercises in Futility," both of which have solidified their reputation as leaders in modern black metal.<br><br></a>
+                    <a>Mgła's live performances are known for their raw intensity and minimalistic stage presence, allowing their music to speak for itself. Their ability to create a profound and immersive experience in a live setting has earned them a devoted following in the underground metal community.<br><br></a>   
+                </div>
+            </div>
+        </div>
+        <div class="menu-line"></div>
+        <div class="band-sector">
+            <div class="band-box">
+                <div class="band-text">
+                    <h1 class="title">5. Furia</h1>
+                    <a>Furia is a Polish black metal band that has gained recognition for its avant-garde and experimental approach to the genre. Hailing from Lublin, Poland, the band was formed in 2003 and has since carved out a unique niche within the broader spectrum of black metal.<br><br></a>
+                    <a>One of Furia's distinguishing features is their willingness to push the boundaries of black metal. While rooted in the genre's traditional aggression and darkness, Furia incorporates a wide range of unconventional elements into their music. This includes experimental soundscapes, spoken word passages, and unexpected instrumentation, all of which contribute to a sense of unpredictability and depth in their compositions.<br><br></a>
+                    <a>Lyrically, Furia's themes are introspective and often delve into existential and philosophical concepts. Their thought-provoking and enigmatic lyrics add an additional layer of complexity to their music, inviting listeners to explore profound and introspective themes.<br><br></a>
+                    <a>Furia's live performances are known for their intensity and emotional depth. They bring their experimental vision to the stage, creating a captivating and immersive experience for their audience.<br><br></a>
+                    <a>Over the years, Furia has released several albums that have garnered critical acclaim within the metal community. Albums like "Księżyc milczy luty" and "Nocel" have showcased their ability to challenge genre conventions while maintaining a distinct black metal core.<br><br></a>
+                    <a>In the realm of Polish black metal, Furia stands out as a band that is unafraid to take risks and explore new artistic territories. Their commitment to pushing the boundaries of the genre has earned them a dedicated following and solidified their place as an influential force in the contemporary black metal landscape.<br><br></a>
+                </div>
+                <div class="band-image">
+                    <img src="images/band5.png"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-gradient">
+            <div class="footer-content">
+                <img src="images/sentino.png"/>
+                <a class="title">pikoseq9</a>
+            </div>
+        </div>
+    </footer>
+    <div class="menu-line"></div>
+</body>
+</html>
